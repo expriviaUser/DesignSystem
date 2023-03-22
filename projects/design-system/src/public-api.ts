85b655b297d8538c97bba735717c3dfbe3559aaa
Daniele Corti <daniele.corti@exprivia.com>
2023-03-22 17:57:16 +0100

    up

--- projects/design-system/src/public-api.ts
+++ projects/design-system/src/public-api.ts
@@ -4,5 +4,5 @@
 
 export * from './lib/design-system.module';
 
-export * from './lib/atoms/tag/lib-tag.module';
 export * from './lib/atoms/tag/components/tag.component';
+export * from './lib/atoms/tag/lib-tag.module';
