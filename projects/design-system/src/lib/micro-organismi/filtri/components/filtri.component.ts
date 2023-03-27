1e18793ccf958340b918f6a962a2023582e9aa75
Daniele Corti <daniele.corti@exprivia.com>
2023-03-27 17:22:55 +0200

    up

---
+++ projects/design-system/src/lib/micro-organismi/filtri/components/filtri.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, OnInit, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { DropdownComponent } from '../../../atoms/dropdown/components/dropdown.component';
+import { DropdownType } from '../../../atoms/dropdown/models/dropdown.model';
+
+@Component({
+    selector: 'lib-filtri',
+    templateUrl: './filtri.component.html',
+    styleUrls: ['./filtri.component.scss']
+})
+export class FiltriComponent implements OnInit {
+
+    @Input() dropdownValues: DropdownType[][] = [];
+
+    @Output() filterValues: EventEmitter<string[]> = new EventEmitter<string[]>();
+
+    @ViewChildren('dropdown') dropdown!: QueryList<DropdownComponent>;
+
+    chipsList: { value: string, dropdownIndex: number }[] = [];
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    createChip(event: { originalEvent: PointerEvent, value: DropdownType }, dropdownIndex: number): void {
+        if (event.value) {
+            this.chipsList = this.chipsList.filter(chip => chip.dropdownIndex != dropdownIndex);
+            this.chipsList.push({ value: event.value.code, dropdownIndex: dropdownIndex });
+            this.chipsList.sort((a, b) => a.dropdownIndex - b.dropdownIndex);
+            const VALUES = this.chipsList.map((chip) => chip.value);
+            this.filterValues.emit(VALUES);
+        }
+    }
+
+    resetDropdown(chipIndex: number, chipValue: { value: string, dropdownIndex: number }): void {
+        // this.dropdown.toArray()[chipValue.dropdownIndex].resetDropdown();
+    }
+
+}
