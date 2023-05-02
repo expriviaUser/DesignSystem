eade45c52fbea9ebbdabd1530c14516940d396f9
Simone Giannuario <simone.giannuario@exprivia.com>
2023-05-02 10:17:58 +0200

    fixed date filters

--- projects/design-system/src/lib/micro-organismi/filters/components/filters.component.ts
+++ projects/design-system/src/lib/micro-organismi/filters/components/filters.component.ts
@@ -9,7 +9,7 @@ import {
 } from '@angular/core';
 import { TreeSelectModel } from "../../../atoms/tree-select/models/tree-select.model";
 import { FiltersModel, FiltersResult } from "../models/filters.model";
-import {CalendarComponent} from "../../../atoms/calendar/components/calendar.component";
+import { CalendarComponent } from "../../../atoms/calendar/components/calendar.component";
 
 @Component({
     selector: 'lib-filters',
@@ -27,7 +27,7 @@ export class FiltersComponent implements OnInit {
     chipsList: { value: string, dropdownIndex: number, field: string, data: string | number | Array<object>, type: "treeselect" | "calendar" }[] = [];
     chipsExport: any = {};
 
-    resetCalendarValue: Array<Date> = [];
+    resetCalendarValue!: Array<Date> | null;
 
     @ViewChildren('calendar') calendar!: QueryList<CalendarComponent>;
 
@@ -55,29 +55,30 @@ export class FiltersComponent implements OnInit {
         }
     }
 
-  createCalendarChip(event: Array<object>, dropdownIndex: number, dropdownOption: FiltersModel, calendar: CalendarComponent): void {
-      //verifico che non ci sia già la chip per il selettore specificato
-      const EXIST = this.chipsList.some((d: any) => d.dropdownIndex == dropdownIndex);
-      if (event[0] && event[1]) {
-      const DATE_RANGE = `${dropdownOption.placeholder}: ${event[0].toLocaleString().split(',')[0]} - ${event[1].toLocaleString().split(',')[0]}`;
-      if(EXIST) {
-        //elimina quello esistente
-        const C_INDEX = this.chipsList.findIndex(c => c.dropdownIndex == dropdownIndex);
-        this.chipsList.splice(C_INDEX, 1);
-      }
-      //Crea il chip
-      this.chipsList.push({ value: DATE_RANGE, dropdownIndex: dropdownIndex, field: dropdownOption.field, data: event, type: "calendar" });
-      this.chipsList.sort((a, b) => a.dropdownIndex - b.dropdownIndex);
-      //Emette il valore
-      this.chipsExport[dropdownOption.field] = event;
-      this.filterValues.emit(this.chipsExport);
-      this.resetCalendarValue = [];
-      //const CAL = this.calendar.toArray();
-      //const CAL_INDEX = CAL.findIndex(c => c.value == event.node.label);
-        //console.log(CAL);
-      calendar.toggleCalendar();
-      }
-  }
+    createCalendarChip(event: Array<object>, dropdownIndex: number, dropdownOption: FiltersModel, calendar: CalendarComponent): void {
+        //verifico che non ci sia già la chip per il selettore specificato
+        const EXIST = this.chipsList.some((d: any) => d.dropdownIndex == dropdownIndex);
+        if (event[0] && event[1]) {
+            const DATE_RANGE = `${dropdownOption.placeholder}: ${event[0].toLocaleString().split(',')[0]} - ${event[1].toLocaleString().split(',')[0]}`;
+            if (EXIST) {
+                //elimina quello esistente
+                const C_INDEX = this.chipsList.findIndex(c => c.dropdownIndex == dropdownIndex);
+                this.chipsList.splice(C_INDEX, 1);
+            }
+            //Crea il chip
+            this.chipsList.push({ value: DATE_RANGE, dropdownIndex: dropdownIndex, field: dropdownOption.field, data: event, type: "calendar" });
+            this.chipsList.sort((a, b) => a.dropdownIndex - b.dropdownIndex);
+            //Emette il valore
+            this.chipsExport[dropdownOption.field] = event;
+            this.filterValues.emit(this.chipsExport);
+            //this.resetCalendarValue = null;
+            calendar.pcalendar.value = null;
+            //const CAL = this.calendar.toArray();
+            //const CAL_INDEX = CAL.findIndex(c => c.value == event.node.label);
+            //console.log(CAL);
+            calendar.toggleCalendar();
+        }
+    }
 
     unselectOption(event: { originalEvent: PointerEvent, node: TreeSelectModel }, dropdownIndex: number, dropdownField: string): void {
         const C_INDEX = this.chipsList.findIndex(c => c.value == event.node.label);
@@ -89,22 +90,22 @@ export class FiltersComponent implements OnInit {
         this.selectedValues[dropdownIndex].splice(N_INDEX, 1);
     }
 
-    resetDropdown( chipIndex: number, chipValue: { value: string, dropdownIndex: number, field: string, data: string | number | Array<object>, type: "treeselect" | "calendar" }): void {
+    resetDropdown(chipIndex: number, chipValue: { value: string, dropdownIndex: number, field: string, data: string | number | Array<object>, type: "treeselect" | "calendar" }): void {
         this.chipsList.splice(chipIndex, 1);
         const E_INDEX = this.chipsExport[chipValue.field].findIndex((d: string) => d == chipValue.data); //add data to chipValue
 
 
-      if(chipValue.type == "treeselect") {
-        this.chipsExport[chipValue.field].splice(E_INDEX, 1);
-        const N_INDEX = this.selectedValues[chipValue.dropdownIndex].findIndex(n => n.label == chipValue.value);
-        this.selectedValues[chipValue.dropdownIndex].splice(N_INDEX, 1);
-        this.selectedValues[chipValue.dropdownIndex] = [...this.selectedValues[chipValue.dropdownIndex]];
-      }
-      else if(chipValue.type == "calendar") {
-        this.chipsExport[chipValue.field] = [];
-        this.resetCalendarValue = [];
-      }
-      this.filterValues.emit(this.chipsExport);
+        if (chipValue.type == "treeselect") {
+            this.chipsExport[chipValue.field].splice(E_INDEX, 1);
+            const N_INDEX = this.selectedValues[chipValue.dropdownIndex].findIndex(n => n.label == chipValue.value);
+            this.selectedValues[chipValue.dropdownIndex].splice(N_INDEX, 1);
+            this.selectedValues[chipValue.dropdownIndex] = [...this.selectedValues[chipValue.dropdownIndex]];
+        }
+        else if (chipValue.type == "calendar") {
+            this.chipsExport[chipValue.field] = [];
+            //this.resetCalendarValue = null;
+        }
+        this.filterValues.emit(this.chipsExport);
     }
 
 
