57d0b59e3fd5594ad776dc253c01db0e3db6c6c9
Simone Giannuario <simone.giannuario@external.eni.com>
2023-09-04 11:13:48 +0200

    fixed search bar

--- projects/design-system/src/lib/molecole/header-items/components/search-bar/search-bar.component.ts
+++ projects/design-system/src/lib/molecole/header-items/components/search-bar/search-bar.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'lib-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+    selector: 'lib-search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
     searchForm: FormGroup = this.fb.group({
@@ -12,5 +12,11 @@ export class SearchBarComponent {
         text: ['']
     })
 
-    constructor(private fb: FormBuilder) {}
+    @Output() onSearch = new EventEmitter<{ category: string, text: string }>();
+
+    constructor(private fb: FormBuilder) { }
+
+    search() {
+        this.onSearch.emit({ category: this.searchForm.get('categorie')?.value, text: this.searchForm.get('text')?.value });
+    }
 }
