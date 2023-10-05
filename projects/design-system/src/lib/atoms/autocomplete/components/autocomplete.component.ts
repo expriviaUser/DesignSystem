b7fa4a916a661d12d293639d051fbe6ac020be63
Simone Giannuario <simone.giannuario@external.eni.com>
2023-10-05 17:38:55 +0200

    added lazy on autocomplete

--- projects/design-system/src/lib/atoms/autocomplete/components/autocomplete.component.ts
+++ projects/design-system/src/lib/atoms/autocomplete/components/autocomplete.component.ts
@@ -3,116 +3,122 @@ import { AbstractControl, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms'
 import { FilterService } from "primeng/api";
 
 @Component({
-    selector: 'lib-autocomplete',
-    templateUrl: './autocomplete.component.html',
-    styleUrls: ['./autocomplete.component.scss'],
-    providers: [{
-        provide: NG_VALUE_ACCESSOR,
-        useExisting: forwardRef(() => AutocompleteComponent),
-        multi: true
-    }, FilterService]
+  selector: 'lib-autocomplete',
+  templateUrl: './autocomplete.component.html',
+  styleUrls: ['./autocomplete.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => AutocompleteComponent),
+    multi: true
+  }, FilterService]
 })
 export class AutocompleteComponent {
-    @Input() valueAutocomplete: string[] | any[] = [];
-    @Input() placeholder: string = '';
-    @Input() value: any;
-    @Input() label: string = '';
-    @Input() appendTo!: string;
-    @Input() icon: string = '';
-    @Input() disabled: boolean = false;
-    @Input() showClear: boolean = false;
-    @Input() minLength: number = 3;
-    @Input() control: AbstractControl = new FormControl();
-    @Input() field: string = '';
+  @Input() valueAutocomplete: string[] | any[] = [];
+  @Input() placeholder: string = '';
+  @Input() value: any;
+  @Input() label: string = '';
+  @Input() appendTo!: string;
+  @Input() icon: string = '';
+  @Input() disabled: boolean = false;
+  @Input() showClear: boolean = false;
+  @Input() minLength: number = 3;
+  @Input() control: AbstractControl = new FormControl();
+  @Input() field: string = '';
+  @Input() lazy: boolean = false;
 
-    @Output() selectedValue: EventEmitter<any> = new EventEmitter<any>();
+  @Output() selectedValue: EventEmitter<any> = new EventEmitter<any>();
+  @Output() emitLazyValue: EventEmitter<string> = new EventEmitter<string>();
 
-    private onChange: any = () => { }
-    private onTouch: any = () => { }
-    protected filteredList: any[] = [];
+  private onChange: any = () => { }
+  private onTouch: any = () => { }
+  protected filteredList: any[] = [];
 
-    get haveError() {
-        return this.control && this.control.errors && (!this.control.pristine || this.control.touched);
-    }
+  get haveError() {
+    return this.control && this.control.errors && (!this.control.pristine || this.control.touched);
+  }
 
-    constructor() { }
+  constructor() { }
 
-    protected filter(event: any) {
-        if (event.query.length > this.minLength - 1) {
-            let filtered: any[] = [];
-            let query = event.query.toString();
-            for (let i = 0; i < this.valueAutocomplete.length; i++) {
-                let value = this.valueAutocomplete[i];
-                if (!this.field && typeof value === 'string') {
-                    if (value.toLowerCase().includes(query.toLowerCase())) {
-                        filtered.push(value);
-                    }
-                } else {
-                    if (value[this.field].toLowerCase().includes(query.toLowerCase())) {
-                        filtered.push(value);
-                    }
-                }
+  protected filter(event: any) {
+    if (!this.lazy) {
+      if (event.query.length > this.minLength - 1) {
+        let filtered: any[] = [];
+        let query = event.query.toString();
+        for (let i = 0; i < this.valueAutocomplete.length; i++) {
+          let value = this.valueAutocomplete[i];
+          if (!this.field && typeof value === 'string') {
+            if (value.toLowerCase().includes(query.toLowerCase())) {
+              filtered.push(value);
             }
-            this.filteredList = filtered;
+          } else {
+            if (value[this.field].toLowerCase().includes(query.toLowerCase())) {
+              filtered.push(value);
+            }
+          }
         }
+        this.filteredList = filtered;
+      }
+    } else {
+      this.emitLazyValue.emit(event.query.toString());
     }
+  }
 
-    // this method sets the value programmatically
-    protected writeValue(value: string) {
-        this.value = value;
-    }
+  // this method sets the value programmatically
+  protected writeValue(value: string) {
+    this.value = value;
+  }
 
-    // set UI element value changes emit function
-    protected registerOnChange(fn: any) {
-        this.onChange = fn
-    }
+  // set UI element value changes emit function
+  protected registerOnChange(fn: any) {
+    this.onChange = fn
+  }
 
-    // set touching element emit function
-    protected registerOnTouched(fn: any) {
-        this.onTouch = fn
-    }
+  // set touching element emit function
+  protected registerOnTouched(fn: any) {
+    this.onTouch = fn
+  }
 
-    // upon disabled statu change, this method gets triggered
-    protected setDisabledState(isDisabled: boolean): void {
-        this.disabled = isDisabled;
-    }
+  // upon disabled statu change, this method gets triggered
+  protected setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 
-    // upon UI element value change, this method gets triggered
-    protected emitValue(event: any, type?: string) {
-        this.value = event;
-        this.onChange(event);
-        this.selectedValue.emit(event);
-    }
+  // upon UI element value change, this method gets triggered
+  protected emitValue(event: any, type?: string) {
+    this.value = event;
+    this.onChange(event);
+    this.selectedValue.emit(event);
+  }
 
-    protected emitValueIfExist(event: any) {
-        if (!this.field) {
-            let valueLowerCase: string[] = this.valueAutocomplete.map(item => item.toLowerCase());
-            if (valueLowerCase && valueLowerCase.includes(event.target.value.toLowerCase())) {
-                this.value = this.valueAutocomplete.filter(item => item.toLowerCase() === event.target.value.toLowerCase())[0];
-                this.control.setErrors(null);
-                this.onChange(this.value);
-                this.selectedValue.emit(this.value);
-            }
-            else if (this.value) {
-                this.onChange("");
-                this.selectedValue.emit("");
-                this.control.setErrors({ 'incorrect': true });
-                this.control.markAsTouched();
-            }
-        } else {
-            if (this.valueAutocomplete.filter(item => typeof item[this.field] === 'string' && typeof event.target.value === 'string' && item[this.field].toLowerCase() === event.target.value.toLowerCase()).length > 0) {
-                this.value = this.valueAutocomplete.filter(item => typeof item[this.field] === 'string' && typeof event.target.value === 'string' && item[this.field].toLowerCase() === event.target.value.toLowerCase())[0];
-                this.control.setErrors(null);
-                this.onChange(this.value);
-                this.selectedValue.emit(this.value);
-            }
-            else if (this.value) {
-                this.onChange("");
-                this.selectedValue.emit("");
-                this.control.setErrors({ 'incorrect': true });
-                this.control.markAsTouched();
-            }
-        }
+  protected emitValueIfExist(event: any) {
+    if (!this.field) {
+      let valueLowerCase: string[] = this.valueAutocomplete.map(item => item.toLowerCase());
+      if (valueLowerCase && valueLowerCase.includes(event.target.value.toLowerCase())) {
+        this.value = this.valueAutocomplete.filter(item => item.toLowerCase() === event.target.value.toLowerCase())[0];
+        this.control.setErrors(null);
+        this.onChange(this.value);
+        this.selectedValue.emit(this.value);
+      }
+      else if (this.value) {
+        this.onChange("");
+        this.selectedValue.emit("");
+        this.control.setErrors({ 'incorrect': true });
+        this.control.markAsTouched();
+      }
+    } else {
+      if (this.valueAutocomplete.filter(item => typeof item[this.field] === 'string' && typeof event.target.value === 'string' && item[this.field].toLowerCase() === event.target.value.toLowerCase()).length > 0) {
+        this.value = this.valueAutocomplete.filter(item => typeof item[this.field] === 'string' && typeof event.target.value === 'string' && item[this.field].toLowerCase() === event.target.value.toLowerCase())[0];
+        this.control.setErrors(null);
+        this.onChange(this.value);
+        this.selectedValue.emit(this.value);
+      }
+      else if (this.value) {
+        this.onChange("");
+        this.selectedValue.emit("");
+        this.control.setErrors({ 'incorrect': true });
+        this.control.markAsTouched();
+      }
     }
+  }
 
 }
--- projects/design-system/src/lib/atoms/input-form/components/input-form.component.ts
+++ projects/design-system/src/lib/atoms/input-form/components/input-form.component.ts
@@ -4,153 +4,154 @@ import { ControlContainer, ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR,
 import { CheckBoxModel } from "../../checkbox/models/checkbox.model";
 
 @Component({
-    selector: "lib-input-form",
-    templateUrl: "./input-form.component.html",
-    styleUrls: ["./input-form.component.scss"],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => InputFormComponent),
-            multi: true,
-        },
-    ],
+  selector: "lib-input-form",
+  templateUrl: "./input-form.component.html",
+  styleUrls: ["./input-form.component.scss"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputFormComponent),
+      multi: true,
+    },
+  ],
 })
 export class InputFormComponent implements ControlValueAccessor {
-    @Input() value!: string;
-    @Input() valueInput: any[] = [];
-    @Input() checkboxValue!: CheckBoxModel[];
-    @Input() clear: boolean = false;
-    @Input() appendTo!: string;
-    @Input() icon: string = "";
-    @Input() iconPos: string = "";
-    @Input() sortParam: string = "nome";
-    @Input() label: string = "";
-    @Input() type: string = "text";
-    @Input() error: boolean = false;
-    @Input() calendarShowIcon: boolean = false;
-    @Input() calendarDropdown: boolean = false;
-    @Input() inputDisabled: boolean = false;
-    @Input() inputFiltered: boolean = false;
-    @Input() placeholder: string = "Inserisci un valore";
-    @Input() formControlName: string = "";
-    @Input() field: string = '';
-    @Input() fieldAutocompleteCard: string[] = [];
-    @Input() inlineCal: boolean = false;
-    @Input() selectionType: string = 'single';
-    @Input() defaultDateFormat: string = 'dd-mm-yy';
-    @Input() minDate!: Date;
-    @Input() maxDate!: Date;
-    @Input() showButtonBar!: boolean;
-    @Input() readonly: boolean = false;
-    @Input() actionIcon: boolean = false;
-    @Input() multiSelectFilterActive: boolean = true;
-    @Input() minLenghtDigits: number = 3;
-    @Input() fileAcceptExtensions!: string;
-    @Input() maxFileSize!: number;
-    @Input() errorTemplate!: TemplateRef<any>;
+  @Input() value!: string;
+  @Input() valueInput: any[] = [];
+  @Input() checkboxValue!: CheckBoxModel[];
+  @Input() clear: boolean = false;
+  @Input() appendTo!: string;
+  @Input() icon: string = "";
+  @Input() iconPos: string = "";
+  @Input() sortParam: string = "nome";
+  @Input() label: string = "";
+  @Input() type: string = "text";
+  @Input() error: boolean = false;
+  @Input() calendarShowIcon: boolean = false;
+  @Input() calendarDropdown: boolean = false;
+  @Input() inputDisabled: boolean = false;
+  @Input() inputFiltered: boolean = false;
+  @Input() placeholder: string = "Inserisci un valore";
+  @Input() formControlName: string = "";
+  @Input() field: string = '';
+  @Input() fieldAutocompleteCard: string[] = [];
+  @Input() inlineCal: boolean = false;
+  @Input() selectionType: string = 'single';
+  @Input() defaultDateFormat: string = 'dd-mm-yy';
+  @Input() minDate!: Date;
+  @Input() maxDate!: Date;
+  @Input() showButtonBar!: boolean;
+  @Input() readonly: boolean = false;
+  @Input() actionIcon: boolean = false;
+  @Input() multiSelectFilterActive: boolean = true;
+  @Input() minLenghtDigits: number = 3;
+  @Input() fileAcceptExtensions!: string;
+  @Input() maxFileSize!: number;
+  @Input() errorTemplate!: TemplateRef<any>;
 
-    //@Input() formControl: FormControl = new FormControl();
-    @Output() selectedValue: EventEmitter<any> = new EventEmitter<any>();
-    @Output() iconClicked: EventEmitter<any> = new EventEmitter<any>();
+  //@Input() formControl: FormControl = new FormControl();
+  @Output() selectedValue: EventEmitter<any> = new EventEmitter<any>();
+  @Output() iconClicked: EventEmitter<any> = new EventEmitter<any>();
+  @Output() autocompleteLazyValue: EventEmitter<string> = new EventEmitter<string>();
 
-    disabled: boolean = false;
-    touched = false;
+  disabled: boolean = false;
+  touched = false;
 
-    onChange: any = () => { };
-    onTouch: any = () => { };
+  onChange: any = () => { };
+  onTouch: any = () => { };
 
-    constructor(
-        private controlContainer: ControlContainer,
-        private datePipe: DatePipe
-    ) { }
+  constructor(
+    private controlContainer: ControlContainer,
+    private datePipe: DatePipe
+  ) { }
 
-    get control() {
-        return this.controlContainer.control!.get(
-            this.formControlName
-        ) as FormControl;
+  get control() {
+    return this.controlContainer.control!.get(
+      this.formControlName
+    ) as FormControl;
+  }
+
+  get haveError() {
+    return (
+      this.control &&
+      this.control.errors &&
+      (!this.control.pristine || this.control.touched)
+    );
+  }
+
+  get required() {
+    return (
+      this.control &&
+      this.control.hasValidator(Validators.required)
+    );
+  }
+  get haveErrorParent() {
+    return (
+      this.control.parent &&
+      this.control.parent.errors &&
+      (!this.control.pristine || this.control.touched)
+    );
+  }
+
+  getValueControl() {
+    if (this.control!.value) {
+      try {
+        return this.datePipe.transform(this.control.value, "dd/MM/yyyy")!;
+      } catch {
+        return this.control!.value;
+      }
+    } else {
+      return "--";
     }
+  }
 
-    get haveError() {
-        return (
-            this.control &&
-            this.control.errors &&
-            (!this.control.pristine || this.control.touched)
-        );
+  // this method sets the value programmatically
+  writeValue(value: string) {
+    this.value = value;
+  }
+
+  // set UI element value changes emit function
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  // set touching element emit function
+  registerOnTouched(fn: any) {
+    this.onTouch = fn;
+  }
+
+  // upon disabled statu change, this method gets triggered
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  // upon UI element value change, this method gets triggered
+  emitValue(event: any, type?: string) {
+    if (type) {
+      if ((event && type == 'autocomplete') || type !== "autocomplete")
+        this.value = event;
+
+      if (type == 'choose-file')
+        this.value = event.files;
+
+      this.onChange(event);
+      this.selectedValue.emit(event);
+
+    } else {
+      this.value = event.target.value;
+      this.onChange(event.target.value);
+      this.selectedValue.emit(event);
     }
+  }
 
-    get required() {
-        return (
-            this.control &&
-            this.control.hasValidator(Validators.required)
-        );
-    }
-    get haveErrorParent() {
-        return (
-            this.control.parent &&
-            this.control.parent.errors &&
-            (!this.control.pristine || this.control.touched)
-        );
-    }
+  resetCalendarValue() {
+    this.control.reset();
+    this.value = '';
+    this.onChange(this.value);
+    this.selectedValue.emit(this.value);
+  }
 
-    getValueControl() {
-        if (this.control!.value) {
-            try {
-                return this.datePipe.transform(this.control.value, "dd/MM/yyyy")!;
-            } catch {
-                return this.control!.value;
-            }
-        } else {
-            return "--";
-        }
-    }
-
-    // this method sets the value programmatically
-    writeValue(value: string) {
-        this.value = value;
-    }
-
-    // set UI element value changes emit function
-    registerOnChange(fn: any) {
-        this.onChange = fn;
-    }
-
-    // set touching element emit function
-    registerOnTouched(fn: any) {
-        this.onTouch = fn;
-    }
-
-    // upon disabled statu change, this method gets triggered
-    setDisabledState(isDisabled: boolean): void {
-        this.disabled = isDisabled;
-    }
-
-    // upon UI element value change, this method gets triggered
-    emitValue(event: any, type?: string) {
-        if (type) {
-            if ((event && type == 'autocomplete') || type !== "autocomplete")
-                this.value = event;
-
-            if (type == 'choose-file')
-                this.value = event.files;
-
-            this.onChange(event);
-            this.selectedValue.emit(event);
-
-        } else {
-            this.value = event.target.value;
-            this.onChange(event.target.value);
-            this.selectedValue.emit(event);
-        }
-    }
-
-    resetCalendarValue() {
-        this.control.reset();
-        this.value = '';
-        this.onChange(this.value);
-        this.selectedValue.emit(this.value);
-    }
-
-    iconClick() {
+  iconClick() {
     this.iconClicked.emit();
   }
 }
