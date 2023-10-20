5eefa2b219d34be260d21e96bea413ea0da9d672
Simone Giannuario <simone.giannuario@external.eni.com>
2023-10-20 09:32:48 +0200

    fixed choose file

--- projects/design-system/src/lib/atoms/choose-file/components/choose-file.component.ts
+++ projects/design-system/src/lib/atoms/choose-file/components/choose-file.component.ts
@@ -1,92 +1,97 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild, forwardRef } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild, forwardRef } from '@angular/core';
 import { AbstractControl, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { FileUpload } from 'primeng/fileupload';
 
 @Component({
-    selector: 'lib-choose-file',
-    templateUrl: './choose-file.component.html',
-    styleUrls: ['./choose-file.component.scss'],
-    providers: [{
-        provide: NG_VALUE_ACCESSOR,
-        useExisting: forwardRef(() => ChooseFileComponent),
-        multi: true
-    }]
+  selector: 'lib-choose-file',
+  templateUrl: './choose-file.component.html',
+  styleUrls: ['./choose-file.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => ChooseFileComponent),
+    multi: true
+  }]
 })
-export class ChooseFileComponent {
-    @Input() label!: string;
-    @Input() acceptExtensions: string = 'image/*';
-    @Input() maxFileSize!: number;
-    @Input() icon!: string;
-    @Input() disabled = false;
-    @Input() control: AbstractControl = new FormControl();
-    @Input() value: any;
-    @Input() multiple: boolean = true;
-    @Output() onLoadFile: EventEmitter<any> = new EventEmitter<any>();
-    protected arrayFiles: File[] = [];
+export class ChooseFileComponent implements OnChanges {
+  @Input() label!: string;
+  @Input() acceptExtensions: string = 'image/*';
+  @Input() maxFileSize!: number;
+  @Input() icon!: string;
+  @Input() disabled = false;
+  @Input() control: AbstractControl = new FormControl();
+  @Input() value: any;
+  @Input() multiple: boolean = true;
+  @Output() onLoadFile: EventEmitter<any> = new EventEmitter<any>();
+  protected arrayFiles: File[] = [];
 
-    @ViewChild('uploader', { static: false }) protected uploader!: FileUpload;
+  @ViewChild('uploader', { static: false }) protected uploader!: FileUpload;
 
-    onChange: any = () => { }
-    onTouch: any = () => { }
+  onChange: any = () => { }
+  onTouch: any = () => { }
 
-
-    // get control() {
-    //     return this.controlContainer.control?.get(this.formControlName);
-    // }
-
-    get inputValue(): string {
-        return this.returnFileName();
-    };
-    get haveError() {
-        return this.control && this.control.errors && (!this.control.pristine || this.control.touched);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['value']?.currentValue?.length === 0) {
+      this.clearFile();
     }
+  }
 
-    // this method sets the value programmatically
-    writeValue(value: string) {
-        this.value = value;
-    }
+  // get control() {
+  //     return this.controlContainer.control?.get(this.formControlName);
+  // }
 
-    // set UI element value changes emit function
-    registerOnChange(fn: any) {
-        this.onChange = fn;
-    }
+  get inputValue(): string {
+    return this.returnFileName();
+  };
+  get haveError() {
+    return this.control && this.control.errors && (!this.control.pristine || this.control.touched);
+  }
 
-    // set touching element emit function
-    registerOnTouched(fn: any) {
-        this.onTouch = fn;
-    }
+  // this method sets the value programmatically
+  writeValue(value: string) {
+    this.value = value;
+  }
 
-    // upon disabled statu change, this method gets triggered
-    setDisabledState(isDisabled: boolean): void {
-        this.disabled = isDisabled;
-    }
+  // set UI element value changes emit function
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
 
-    onSelect(event: any) {
-        let filesName: string[] = [];
-        event.currentFiles.forEach((file: File) => {
-            this.arrayFiles.push(file);
-            filesName.push(file.name);
-        })
-        this.uploader.clear();
-        this.onLoadFile.emit(event.currentFiles);
-    }
+  // set touching element emit function
+  registerOnTouched(fn: any) {
+    this.onTouch = fn;
+  }
 
-    clearFile() {
-        this.uploader.clear();
-        this.arrayFiles = [];
-        this.onLoadFile.emit('');
-    }
+  // upon disabled statu change, this method gets triggered
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 
-    returnFileName() {
-        let initValue: string = '';
-        let filesNames = this.arrayFiles.reduce((accumulator, currentValue, index) => accumulator + currentValue.name + (this.arrayFiles.length - 1 > index ? ', ' : ''), initValue);
+  onSelect(event: any) {
+    let filesName: string[] = [];
+    event.currentFiles.forEach((file: File) => {
+      this.arrayFiles.push(file);
+      filesName.push(file.name);
+    })
+    this.uploader.clear();
+    this.onLoadFile.emit(event.currentFiles);
+  }
 
-        return filesNames;
-    }
+  clearFile() {
+    this.uploader.clear();
+    this.arrayFiles = [];
+    this.onLoadFile.emit('');
+  }
 
-    uploadFiles() {
-        this.uploader.upload();
-    }
+  returnFileName() {
+    let initValue: string = '';
+    let filesNames = this.arrayFiles.reduce((accumulator, currentValue, index) => accumulator + currentValue.name + (this.arrayFiles.length - 1 > index ? ', ' : ''), initValue);
+
+    return filesNames;
+  }
+
+  uploadFiles() {
+    this.uploader.upload();
+  }
 
 
 
