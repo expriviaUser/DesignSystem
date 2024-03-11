0b43dfd9da1c91199f8197099120d1fc27dfca4a
Simone Giannuario <simone.giannuario@external.eni.com>
2024-03-11 15:44:59 +0100

    fix

--- projects/design-system/src/lib/atoms/treemenu/components/treemenu.component.ts
+++ projects/design-system/src/lib/atoms/treemenu/components/treemenu.component.ts
@@ -1,28 +1,41 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { TreeMenu } from '../models/treemenu.model';
+import { TreeSelectModel } from '@expriviauser/design-system';
 
 @Component({
-    selector: 'lib-treemenu',
-    templateUrl: './treemenu.component.html',
-    styleUrls: ['./treemenu.component.scss']
+  selector: 'lib-treemenu',
+  templateUrl: './treemenu.component.html',
+  styleUrls: ['./treemenu.component.scss']
 })
 export class TreemenuComponent {
-    @Input() items!: TreeMenu[];
-    @Input() filter: boolean = false;
-    @Input() scrollHeight!: string;
-    @Input() checkboxOnlyLeaf: boolean = false;
-    @Input() propagateSelectionUp: boolean = true;
-    @Input() propagateSelectionDown: boolean = true;
-    @Input() selectionMode: string = 'single';
-    @Input() filterPlaceholder: string = 'Search';
-    @Input() filterMode: string = 'lenient';
-    @Input() selected!: any;
+  @Input() items!: TreeMenu[];
+  @Input() filter: boolean = false;
+  @Input() scrollHeight!: string;
+  @Input() checkboxOnlyLeaf: boolean = false;
+  @Input() propagateSelectionUp: boolean = true;
+  @Input() propagateSelectionDown: boolean = true;
+  @Input() selectionMode: string = 'single';
+  @Input() filterPlaceholder: string = 'Search';
+  @Input() filterMode: string = 'lenient';
+  @Input() selected!: any;
 
-    @Output() selectedChange: EventEmitter<TreeMenu[]> = new EventEmitter<TreeMenu[]>();
+  @Output() selectedChange: EventEmitter<TreeMenu[]> = new EventEmitter<TreeMenu[]>();
 
-    changedSelection(event: any) {
-        this.selectedChange.emit(event);
-    }
+  changedSelection(event: any) {
+    this.selectedChange.emit(event);
+  }
+
+  optionEmit(event: { originalEvent: PointerEvent, node: TreeSelectModel }): void {
+    event.originalEvent.stopPropagation();
+    event.originalEvent.preventDefault();
+    // this.emitSelectedOption.emit(event);
+  }
+
+  unselectEmit(event: { originalEvent: PointerEvent, node: TreeSelectModel }): void {
+    event.originalEvent.stopPropagation();
+    event.originalEvent.preventDefault();
+    //this.emitUnselect.emit(event);
+  }
 
 
 }
