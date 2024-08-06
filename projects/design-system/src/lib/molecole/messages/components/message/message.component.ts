34c1490f9490f2e0137906833d078e8f012536d6
Simone Giannuario <simone.giannuario@external.eni.com>
2024-08-06 15:29:54 +0200

    fix: fixed toast messages without service

--- projects/design-system/src/lib/molecole/messages/components/message/message.component.ts
+++ projects/design-system/src/lib/molecole/messages/components/message/message.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Message } from 'primeng/api';
-// import { Message } from 'primeng/api';
 
 @Component({
-    selector: 'lib-message',
-    templateUrl: './message.component.html',
-    styleUrls: ['./message.component.scss']
+  selector: 'lib-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss']
 })
 export class MessageComponent {
 
-    // msgs1!: Message[];
-    @Input() key: string = '';
-    @Input() enableService: boolean = false;
-    @Input() messages: Message[] = [];
+  // msgs1!: Message[];
+  @Input() key: string = '';
+  @Input() enableService: boolean = false;
+  @Input() messages: Message[] = [];
+  @Input() closable: boolean = true;
+
+  @Output() messagesChange = new EventEmitter<Message[]>();
+  
+
+
+  emitMessagesChange(event: Message[]) {
+    this.messages = event;
+    this.messagesChange.emit(this.messages);
+  }
 
 }
